488d8ef01cd52bba5172b14506e16c1b61ffdd48
Fikre-M <fikreddu@gmail.com>
2025-07-22 00:41:16 -0400

    edit the tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -8,23 +7,3 @@ export default {
 };
 
 
-
-
-
-
-// tailwind.config.js
-// module.exports = {
-//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         sans: ['system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
-//       },
-//       colors: {
-//         background: '#242424',
-//         text: 'rgba(255, 255, 255, 0.87)',
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
